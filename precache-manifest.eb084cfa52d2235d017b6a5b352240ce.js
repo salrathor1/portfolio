2c3d0080ce9d923bb8380786ed4c8f5b1a820b28
salrathor1 <salrathor1@hotmail.com>
2020-06-08 14:50:10 +0100

    Updates

---
+++ precache-manifest.eb084cfa52d2235d017b6a5b352240ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b35833e7246be1e14211cb91ccd78de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eedf46052f425754f40",
+    "url": "/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "5e118e0a5a58c6598595",
+    "url": "/static/css/main.96da3d0f.chunk.css"
+  },
+  {
+    "revision": "6eedf46052f425754f40",
+    "url": "/static/js/2.c5c2127b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c5c2127b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e118e0a5a58c6598595",
+    "url": "/static/js/main.859560e4.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
